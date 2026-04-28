02bca2c04a0057068a9ffbceec25a009f476d258
kanghana1 <qazplm12093@naver.com>
2026-04-28 21:53:28 +0900

    chore: 오타수정

--- 와나/server/src/main/java/umc/server/domain/member/exception/MemberErrorCode.java
+++ 와나/server/src/main/java/umc/server/domain/member/exception/MemberErrorCode.java
@@ -9,7 +9,7 @@ import umc.server.global.exception.code.BaseCode;
 @RequiredArgsConstructor
 public enum MemberErrorCode implements BaseCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_4001", "회원을 찾을 수 없습니다."),,
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_4001", "회원을 찾을 수 없습니다."),
 
     ;
     private final HttpStatus httpStatus;
