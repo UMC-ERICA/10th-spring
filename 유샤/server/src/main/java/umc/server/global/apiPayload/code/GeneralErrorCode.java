6aeb4b94768b53cabbb84dd912d2ddbe0ae6d7b9
oofrog <oofrog0823@gmail.com>
2026-04-28 22:58:17 +0900

    feat: 에러핸들링

--- 유샤/server/src/main/java/umc/server/global/apiPayload/code/GeneralErrorCode.java
+++ 유샤/server/src/main/java/umc/server/global/apiPayload/code/GeneralErrorCode.java
@@ -19,7 +19,10 @@ public enum GeneralErrorCode implements BaseErrorCode {
             "접근이 금지되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "COMMON404_1",
-            "해당 리소스를 찾을 수 없습니다."),;
+            "해당 리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "서버 연결이 끊겼습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
