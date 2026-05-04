154048127098a70588ccbbf243599c5414dfcb1c
oofrog <oofrog0823@gmail.com>
2026-05-04 16:37:42 +0900

    chore: 성공코드 설명 수정

--- 유샤/server/src/main/java/umc/server/domain/mission/exception/code/MissionSuccessCode.java
+++ 유샤/server/src/main/java/umc/server/domain/mission/exception/code/MissionSuccessCode.java
@@ -17,7 +17,7 @@ public enum MissionSuccessCode implements BaseSuccessCode {
             "성공적으로 지역미션진척도를 조회했습니다."),
     MISSION_COMPLETE(HttpStatus.OK,
             "MISSION200_3",
-            "성공적으로 미션이 수정되었습니다.");
+            "미션 complete: false -> true");
 
     private final HttpStatus httpStatus;
     private final String code;
