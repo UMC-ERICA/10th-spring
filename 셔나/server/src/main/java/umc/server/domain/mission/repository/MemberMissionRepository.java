da0976d18c80248da4bde1f6d1cdebcb431e58d3
leesiyeon <sylee3182@gmail.com>
2026-05-05 18:29:39 +0900

    refactor: MemberMissionRepository 조회 로직 @Query 기반으로 전환

--- 셔나/server/src/main/java/umc/server/domain/mission/repository/MemberMissionRepository.java
+++ 셔나/server/src/main/java/umc/server/domain/mission/repository/MemberMissionRepository.java
@@ -1,6 +1,7 @@
 package umc.server.domain.mission.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import umc.server.domain.mission.entity.MemberMission;
 import umc.server.domain.mission.enums.MissionStatus;
@@ -12,8 +13,10 @@ import java.util.Optional;
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
 
     // 특정 회원의 상태별 미션 목록 조회 (진행중/진행완료)
+    @Query("SELECT mm FROM MemberMission mm JOIN FETCH mm.mission WHERE mm.member.id = :memberId AND mm.missionStatus = :status")
     List<MemberMission> findAllByMemberIdAndMissionStatus(Long memberId, MissionStatus status);
 
     // 특정 회원의 특정 미션 기록 찾기
+    @Query("SELECT mm FROM MemberMission mm WHERE mm.member.id = :memberId AND mm.mission.id = :missionId")
     Optional<MemberMission> findByMemberIdAndMissionId(Long memberId, Long missionId);
 }
