eca643eff07bc22e25b9e91df6449eac2e086be4
oofrog <oofrog0823@gmail.com>
2026-05-06 16:46:38 +0900

    refactor: ID생성타입 시퀀스 -> IDENTITY

--- 유샤/server/src/main/java/umc/server/domain/common/entity/Address.java
+++ 유샤/server/src/main/java/umc/server/domain/common/entity/Address.java
@@ -11,7 +11,7 @@ import lombok.*;
 public class Address extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     private String regionUpper;
