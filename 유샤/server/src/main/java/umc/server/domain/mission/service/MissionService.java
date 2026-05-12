3266a26a7ddfcfad83f2cefbd3911dfa40349731
oofrog <oofrog0823@gmail.com>
2026-05-12 09:37:03 +0900

    feat: 워크북 학습

--- 유샤/server/src/main/java/umc/server/domain/mission/service/MissionService.java
+++ 유샤/server/src/main/java/umc/server/domain/mission/service/MissionService.java
@@ -1,13 +1,23 @@
 package umc.server.domain.mission.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import umc.server.domain.common.entity.Address;
 import umc.server.domain.common.repository.AddressRepository;
 import umc.server.domain.mission.converter.MissionConverter;
+import umc.server.domain.mission.dto.MissionReqDTO;
 import umc.server.domain.mission.dto.MissionResDTO;
 import umc.server.domain.mission.entity.Mission;
 import umc.server.domain.mission.repository.MemberMissionRepository;
+import umc.server.domain.mission.repository.MissionRepository;
+import umc.server.domain.store.entity.Store;
+import umc.server.domain.store.exception.StoreException;
+import umc.server.domain.store.exception.code.StoreErrorCode;
+import umc.server.domain.store.repository.StoreRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,6 +29,8 @@ public class MissionService {
 
     private final MemberMissionRepository memberMissionRepository;
     private final AddressRepository addressRepository;
+    private final StoreRepository storeRepository;
+    private final MissionRepository missionRepository;
 
     public MissionResDTO.GetMissionList getMissionList(boolean isCompleted){
 
@@ -42,5 +54,42 @@ public class MissionService {
         Integer progress = memberMissionRepository.findRegionMissionProgress(1L,regionSub);
 
         return MissionConverter.toGetProgressResult(progress);
+    }
+
+    @Transactional
+    public Void create(Long storeId, MissionReqDTO.CreateMission dto) {
+
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(()-> new StoreException(StoreErrorCode.NOT_FOUND));
+
+        Mission mission = MissionConverter.toMission(store,dto);
+
+        missionRepository.save(mission);
+        return null;
+    }
+
+    public MissionResDTO.Pagination<MissionResDTO.GetMission> getMissionListByStore(
+            Long storeId,
+            Integer pageSize,
+            Integer pageNumber,
+            String sort
+    ) {
+        Sort sortInfo;
+        if(sort != null){
+            sortInfo = Sort.by(sort);
+        }else{
+            sortInfo = Sort.by("id").descending();
+        }
+
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sortInfo);
+
+        Page<Mission> missionList = missionRepository.findAllByStore_Id(storeId,pageRequest);
+
+
+        return MissionConverter.toPagination(
+                missionList.map(MissionConverter::toGetMission).toList(),
+                missionList.getNumber(),
+                missionList.getSize()
+        );
     }
 }
--- 유샤/server/src/main/java/umc/server/domain/store/exception/code/StoreErrorCode.java
+++ 유샤/server/src/main/java/umc/server/domain/store/exception/code/StoreErrorCode.java
@@ -1,4 +1,19 @@
 package umc.server.domain.store.exception.code;
 
-public enum StoreErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import umc.server.global.apiPayload.code.BaseErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum StoreErrorCode implements BaseErrorCode {
+
+    NOT_FOUND(HttpStatus.NOT_FOUND,
+            "STORE404_1",
+            "해당 가게가 존재하지 않습니다."),;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
